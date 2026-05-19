2ec78a9bc2af178213af3ba6c78dbb7eea7c1d25
nearha <114181979+nearha@users.noreply.github.com>
2026-05-19 20:40:56 -0300

    Expand constants

--- custom_components/blueiris_ui3/const.py
+++ custom_components/blueiris_ui3/const.py
@@ -1,1 +1,22 @@
 DOMAIN = "blueiris_ui3"
+DATA_CLIENTS = "clients"
+DATA_VIEWS_REGISTERED = "views_registered"
+
+CONF_HOST = "host"
+CONF_PORT = "port"
+CONF_SSL = "ssl"
+CONF_USERNAME = "username"
+CONF_PASSWORD = "password"
+
+DEFAULT_PORT = 80
+DEFAULT_PROFILES = [
+    {"id": "2160p VBR^", "name": "4K VBR"},
+    {"id": "1080p VBR^", "name": "1080p VBR"},
+    {"id": "2160p^", "name": "4K"},
+    {"id": "1440p^", "name": "1440p"},
+    {"id": "1080p^", "name": "1080p"},
+    {"id": "720p^", "name": "720p"},
+    {"id": "480p", "name": "480p"},
+    {"id": "360p", "name": "360p"},
+    {"id": "240p", "name": "240p"},
+]
